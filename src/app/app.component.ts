3a6ce39bfdb661fbf6e061fba1c5d8021b8fc706
anasbellaghzilia <anas.bellaghzilia@gmail.com>
2017-11-09 02:23:37 +0100

    Test Modification -- Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template:
+  `
+  <div>
+    <h1>Hello {{name}}</h1>
+    <p>By Anas Bellaghzilia </p>
+  </div> 
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Word'; }
